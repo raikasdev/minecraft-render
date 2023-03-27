821b5960a3e579107e31cd65d05c6ace9e3f96f1
Roni Äikäs <roni.aikas@gmail.com>
2023-03-27 22:25:10 +0300

    Update render.test.ts

--- src/tests/render.test.ts
+++ src/tests/render.test.ts
@@ -17,7 +17,7 @@ export class RenderTest {
 
     const renderCandidates = pickBlocks(blocks);
 
-    for await (const render of this.minecraftTest.minecraft.render(renderCandidates, {width:16,height:16,animation:false,distance:13})) {
+    for await (const render of this.minecraftTest.minecraft.render(renderCandidates, {width:32,height:32,animation:false,distance:11})) {
       if (!render.buffer) {
         console.log('Rendering skipped ' + render.blockName + ' reason: ' + render.skip!);
         continue;
